5c59efc81d1287cc381eb9e90723b54d4bc192f7
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-27 01:36:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,8 +14,11 @@ class Scraper
   end
   
   def make_courses
-    Course.all.each |course|
-      course.
+    get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
   end
   
