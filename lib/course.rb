a108752751bb9862e4c16eb7d828510be2b83d0d
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-27 00:16:07 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,7 +1,7 @@
 class Course
   attr_accessor :title, :schedule, :description
   @@all = []
-  def initialize(title, schedule, description)
+  def initialize
     @@all << self
   end
   def self.all
